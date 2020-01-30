4ba3cdc245ca1aa403992f91a93a7b01e6a31a1f
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-01-30 18:03:51 -0500

    produced further styling of the weathercontainer component, verfication of length and chars via regex, parameters and if and else statements. Submission of api key, prior coding retrival of data in the application upon search

--- src/components/WeatherContainer/WeatherContainer.js
+++ src/components/WeatherContainer/WeatherContainer.js
@@ -2,10 +2,37 @@ import React, { useState } from "react";
 import "./WeatherContainer.css";
 
 function WeatherContainer() {
-  const [searchQuery, setSearchQuery] = useState();
+  const API_KEY = "6f5f2082b4fd44b3747c6ac39562862d";
+  const [searchQuery, setSearchQuery] = useState("");
+  const [weatherData, setWeatherData] = useState({
+    temperature: null,
+    humidity: null,
+    description: null,
+    city: null
+  });
+
+  const [isValidZipCode, setIsValidZipCode] = useState(true);
 
   function updateSearchQuery(event) {
+    let zipCode = event.target.value;
+    let isValid = validateZipCode(zipCode);
+    setSearchQuery(zipCode);
+
+    if (isValid || zipCode === "" || isValid.length === 5) {
+      setIsValidZipCode(true);
+    } else {
+      setIsValidZipCode(false);
+    }
     setSearchQuery(event.target.value);
+  }
+
+  function validateZipCode(zipCode) {
+    let regex = /[0-9]{5}/;
+    return regex.test(zipCode);
+  }
+
+  function getWeatherData() {
+    //obtain data from api
   }
 
   return (
@@ -17,10 +44,24 @@ function WeatherContainer() {
             placeholder="Enter Zip Code"
             className="search-engine"
             onChange={updateSearchQuery}
+            maxLength="5"
           />
-          <button className="material-icons">search</button>
+          <button onClick={getWeatherData} className="material-icons">
+            search
+          </button>
         </div>
       </header>
+      <p className="error">{isValidZipCode ? "" : "*Invalid Zip Code*"}</p>
+      <div className="weather-info">
+        {weatherData.temperature === null ? (
+          <p>
+            No Weather To Display
+            <i className="material-icons">wb_sunny</i>
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
