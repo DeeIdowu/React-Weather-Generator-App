c051d9b819e228994729eb78b8b5299abcee6e3e
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-01-29 21:45:32 -0500

    implemented styling to weather component and install usestate for the user input of the search engine

--- src/components/WeatherContainer/WeatherContainer.js
+++ src/components/WeatherContainer/WeatherContainer.js
@@ -2,12 +2,22 @@ import React, { useState } from "react";
 import "./WeatherContainer.css";
 
 function WeatherContainer() {
+  const [searchQuery, setSearchQuery] = useState();
+
+  function updateSearchQuery(event) {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="weather-container">
-      <header>
+      <header className="weather-header">
         <h3>Weather Generator</h3>
         <div>
-          <input placeholder="Enter Zip Code" className="search-engine" />
+          <input
+            placeholder="Enter Zip Code"
+            className="search-engine"
+            onChange={updateSearchQuery}
+          />
         </div>
       </header>
     </div>
